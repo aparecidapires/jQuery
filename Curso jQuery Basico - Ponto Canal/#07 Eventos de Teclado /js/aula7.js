6531a25bdf82a4d295a9f9a82c942cfd7879f8f1
aparecida <aparecidapiresbarb@gmail.com>
2016-01-13 18:09:07 -0200

    Formulário: focusin/focusout

---
+++ Curso jQuery Basico - Ponto Canal/#07 Eventos de Teclado /js/aula7.js
@@ -0,0 +1,29 @@
+$(function () {
+
+	$('.place').each(function() {
+		var place = $(this).attr('title');
+		var input = $(this);
+		input
+			.val(place)
+			.css('color', '#ccc')
+			.focusin(function(){
+				input.css('color', '#000');
+				if(input.val() == place){
+					input.val('');
+				}
+			}).focusout(function(){
+				if(input.val() == '') {
+					input.css('color', '#ccc');
+					input.val(place);
+				}
+			});
+
+	});
+
+	var ex = $('.mensagem');
+
+	$('.key').keypress(function() {
+		ex.text($(this).val());	
+	});
+
+});
